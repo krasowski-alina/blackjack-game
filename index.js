8ed92ce756a869c06e121fb361fd672097485611
“krasowski-alina” <alina.krasowski.web@gmail.com>
2023-02-13 21:08:44 -0500

    set the winning condition

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
-const sum = document.querySelector("#sum")
-const startGame = document.querySelector("#startGame")
-const newGame = document.querySelector("#newGame")
+const sumText = document.querySelector("#sum")
+const startGameBtn = document.querySelector("#startGame")
+const newGameBtn = document.querySelector("#newGame")
+let message = document.querySelector("#message")
+let firstCard = 10
+let secondCard = 10
+let sum = firstCard + secondCard
+const cards = []
+
+function startGame(){
+    if(sum === 21) {
+        message.textContent = "You've got Blackjack!"
+    }else if( sum < 21) {
+        message.textContent = "Do you want to draw a new card?"
+    }else {
+        message.textContent = "You're out of the game!"
+    }
+}
+startGame()
