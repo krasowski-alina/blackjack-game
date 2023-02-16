6280ec37b49998d2d5ad7f97da734bcb360bdd2b
“krasowski-alina” <alina.krasowski.web@gmail.com>
2023-02-16 15:50:26 -0500

    added random values for cards, rendered on screen

--- index.js
+++ index.js
@@ -1,19 +1,28 @@
 const sumText = document.querySelector("#sum")
 const startGameBtn = document.querySelector("#startGame")
 const newGameBtn = document.querySelector("#newGame")
-let message = document.querySelector("#message")
-let firstCard = 10
-let secondCard = 10
-let sum = firstCard + secondCard
+const message = document.querySelector("#message")
+const cardsDisplay = document.querySelector("#cardsDisplay")
+let firstCard
+let secondCard
+
 const cards = []
 
 function startGame(){
-    if(sum === 21) {
-        message.textContent = "You've got Blackjack!"
-    }else if( sum < 21) {
+    firstCard = Math.floor(Math.random()  * 13) + 1
+    secondCard = Math.floor(Math.random()  * 13) + 1
+    let sum = firstCard + secondCard
+    if(sum < 21) {
         message.textContent = "Do you want to draw a new card?"
+    }else if( sum === 21) {
+        message.textContent = "You've got Blackjack!"
     }else {
         message.textContent = "You're out of the game!"
     }
+    cardsDisplay.textContent = `Cards: ${firstCard}, ${secondCard}`
+    sumText.textContent = `Sum: ${sum}`
 }
-startGame()
+
+startGameBtn.addEventListener("click", ()=> {
+    startGame()
+})
