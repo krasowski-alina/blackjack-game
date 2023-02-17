ccc1ee7a97e6ad1a040a096e534a3240e01f83a0
“krasowski-alina” <alina.krasowski.web@gmail.com>
2023-02-17 16:40:16 -0500

    assigned values in the startGame function

--- index.js
+++ index.js
@@ -3,35 +3,38 @@ const startGameBtn = document.querySelector("#startGame")
 const newCardBtn = document.querySelector("#newGame")
 const messageElem = document.querySelector("#message")
 const cardsDisplay = document.querySelector("#cardsDisplay")
-let isAlive = true
+let isAlive = false
 let hasBlackjack = false
 let message = ""
-let firstCard = createRandomCard()
-let secondCard = createRandomCard()
-let sum = firstCard + secondCard
-const cards = [firstCard, secondCard]
+let sum = 0
+const cards = []
+console.log(cards)
 
 function startGame(){
+    isAlive = true
+    let firstCard = createRandomCard()
+    let secondCard = createRandomCard()
+    sum = firstCard + secondCard 
+    cards.push(firstCard)
+    cards.push(secondCard)
     renderGame()
-}
+    }
+
 function createRandomCard(){
        let random = Math.floor(Math.random()  * 13) + 1
-       if(random > 10){
-        return 10
-       }else if(random === 1){
-        return 11
-       }else{
-        return  random
-       }
+        if(random > 10){
+            return 10
+        }else if(random === 1){
+            return 11
+        }else{
+            return  random
+        }
 }
 function renderGame(){
-    // firstCard = Math.floor(Math.random()  * 13) + 1
-    // secondCard = Math.floor(Math.random()  * 13) + 1
     cardsDisplay.textContent = `Cards: `
     for(let i = 0; i< cards.length; i++){
         cardsDisplay.textContent +=  `${cards[i]} `
     }
-
     if(sum < 21) {
         message = "Do you want to draw a new card?"
     }else if( sum === 21) {
