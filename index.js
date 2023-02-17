5356b0bc0a8766faf0ff1d36529a413aaf5b6f4a
“krasowski-alina” <alina.krasowski.web@gmail.com>
2023-02-17 16:19:43 -0500

    added opportunity to draw a new card

--- index.js
+++ index.js
@@ -1,28 +1,56 @@
 const sumText = document.querySelector("#sum")
 const startGameBtn = document.querySelector("#startGame")
-const newGameBtn = document.querySelector("#newGame")
-const message = document.querySelector("#message")
+const newCardBtn = document.querySelector("#newGame")
+const messageElem = document.querySelector("#message")
 const cardsDisplay = document.querySelector("#cardsDisplay")
-let firstCard
-let secondCard
-
-const cards = []
+let isAlive = true
+let hasBlackjack = false
+let message = ""
+let firstCard = createRandomCard()
+let secondCard = createRandomCard()
+let sum = firstCard + secondCard
+const cards = [firstCard, secondCard]
 
 function startGame(){
-    firstCard = Math.floor(Math.random()  * 13) + 1
-    secondCard = Math.floor(Math.random()  * 13) + 1
-    let sum = firstCard + secondCard
-    if(sum < 21) {
-        message.textContent = "Do you want to draw a new card?"
-    }else if( sum === 21) {
-        message.textContent = "You've got Blackjack!"
-    }else {
-        message.textContent = "You're out of the game!"
+    renderGame()
+}
+function createRandomCard(){
+       let random = Math.floor(Math.random()  * 13) + 1
+       if(random > 10){
+        return 10
+       }else if(random === 1){
+        return 11
+       }else{
+        return  random
+       }
+}
+function renderGame(){
+    // firstCard = Math.floor(Math.random()  * 13) + 1
+    // secondCard = Math.floor(Math.random()  * 13) + 1
+    cardsDisplay.textContent = `Cards: `
+    for(let i = 0; i< cards.length; i++){
+        cardsDisplay.textContent +=  `${cards[i]} `
     }
-    cardsDisplay.textContent = `Cards: ${firstCard}, ${secondCard}`
+
+    if(sum < 21) {
+        message = "Do you want to draw a new card?"
+    }else if( sum === 21) {
+        message = "You've got Blackjack!"
+        hasBlackjack = true
+    }else {
+        message = "You're out of the game!"
+        isAlive = false
+    }
+    messageElem.textContent = message
     sumText.textContent = `Sum: ${sum}`
 }
 
 startGameBtn.addEventListener("click", ()=> {
     startGame()
+})
+newCardBtn.addEventListener("click", ()=> {
+    let thirdCard = createRandomCard()
+    sum += thirdCard
+    cards.push(thirdCard)
+    renderGame()
 })
